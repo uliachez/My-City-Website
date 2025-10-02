21d6b6c1adb7df4e204f6436a02798a55a7bfae8
Anastasia <n_pishaeva@mail.ru>
2025-10-02 13:01:17 +0300

    header_without_logo

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
-// Пример динамики: кнопка меняет фон сайта (по желанию можно добавить на всех страницах)
-document.addEventListener("DOMContentLoaded", () => {
-  const main = document.querySelector("main");
+// // Пример динамики: кнопка меняет фон сайта (по желанию можно добавить на всех страницах)
+// document.addEventListener("DOMContentLoaded", () => {
+//   const main = document.querySelector("main");
 
-  if (main) {
-    main.addEventListener("click", () => {
-      document.body.style.backgroundColor =
-        document.body.style.backgroundColor === "lightyellow"
-          ? "white"
-          : "lightyellow";
-    });
-  }
-});
+//   if (main) {
+//     main.addEventListener("click", () => {
+//       document.body.style.backgroundColor =
+//         document.body.style.backgroundColor === "lightyellow"
+//           ? "white"
+//           : "lightyellow";
+//     });
+//   }
+// });
